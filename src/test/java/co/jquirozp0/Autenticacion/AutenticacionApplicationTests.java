431fb778eff0f4edbb6ab58a8750cd2d368c4f35
Jquirozp0 <jquirozp@ucentral.edu.co>
2024-08-28 20:45:42 -0500

    Primer commit autennticacion

---
+++ src/test/java/co/jquirozp0/Autenticacion/AutenticacionApplicationTests.java
@@ -0,0 +1,13 @@
+package co.jquirozp0.Autenticacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
